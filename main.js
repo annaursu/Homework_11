62e5ea4e402063f46fa9e3d84146d3d4d9c8f2fd
annaursu <60402005+annaursu@users.noreply.github.com>
2020-03-10 22:06:25 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var weekDays = prompt ("Which weekday is today?");
+switch (weekDays) {
+    case "monday":
+      alert ("Working day");
+      break;
+    case "tuesday":
+      alert ("Working day");
+      break;
+    case "wednesday":
+      alert ("Working day");
+      break;
+    case "thursday":
+      alert ("Working day");
+      break;
+    case "friday":
+      alert ("Working day");
+      break;
+    case "saturday":
+      alert ("Weekend");
+      break;
+    case "sunday":
+      alert ("Weekend");
+      break;
+    default:
+      alert ("Can not define, please try another value");
+
+}
+
